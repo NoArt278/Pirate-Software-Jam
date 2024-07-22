92686d0abf18a535ebec898443e4ffd19f325780
Nat10k <156871636+NoArt278@users.noreply.github.com>
2024-07-22 22:34:13 +0700

    fix : put raycast wall further back

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,6 +7,8 @@ const JUMP_VELOCITY = 3
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _ready():
+	print(collision_layer)
 
 func _physics_process(delta):
 	# Add the gravity.
