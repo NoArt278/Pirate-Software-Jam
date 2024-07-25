6ebe9bbb0caee1c4c4382dba64f8f84ad0b328d3
Nat10k <156871636+NoArt278@users.noreply.github.com>
2024-07-25 18:15:57 +0700

    feat : virtual cursor and prism shape

--- scripts/pickup.gd
+++ scripts/pickup.gd
@@ -1,13 +1,24 @@
 extends Camera3D
 
+@onready var virtual_cursor : TextureRect = %VirtualCursor
 @onready var ray_cast_3d : RayCast3D = $RayCast3D
 const ray_cast_dist : float = 30
+const rotate_sensitivity : float = 0.01
 var curr_moved_object : RigidBody3D = null
 var is_dragging : bool = false
+var is_rotating : bool = false
+var virtual_cursor_pos : Vector2 = Vector2.ZERO
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	virtual_cursor_pos = get_viewport().get_mouse_position()
+
 func _process(delta):
-	ray_cast_3d.target_position = get_viewport().get_camera_3d().project_ray_origin(get_viewport().get_mouse_position()) + get_viewport().get_camera_3d().project_ray_normal(get_viewport().get_mouse_position()) * ray_cast_dist
+	var from_pos = project_ray_origin(virtual_cursor_pos)
+	var end_pos = from_pos + project_ray_normal(virtual_cursor_pos) * ray_cast_dist
+	ray_cast_3d.global_position = from_pos
+	ray_cast_3d.target_position = end_pos
+	virtual_cursor.set_global_position(virtual_cursor_pos)
 	if (ray_cast_3d.is_colliding() and not(is_dragging)) :
 		if (ray_cast_3d.get_collider() is RigidBody3D) : # a movable object
 			curr_moved_object = ray_cast_3d.get_collider()
@@ -19,10 +30,23 @@ func _physics_process(delta):
 		is_dragging = true
 		curr_moved_object.freeze = true
 		curr_moved_object.collision_layer = 2
+	if (Input.is_action_just_released("right_click")) :
+		is_rotating = false
 	if (Input.is_action_pressed("click") and is_dragging) :
-		var new_position : Vector3 = ray_cast_3d.get_collision_point()
-		curr_moved_object.global_position = new_position
+		if (Input.is_action_just_pressed("right_click")) :
+			is_rotating = true
+		elif (!Input.is_action_pressed("right_click")) :
+			var new_position : Vector3 = ray_cast_3d.get_collision_point()
+			curr_moved_object.global_position = new_position
 	elif (Input.is_action_just_released("click") and curr_moved_object != null) :
 		curr_moved_object.freeze = false
 		is_dragging = false
 		curr_moved_object.collision_layer = 1
+
+func _input(event):
+	if event is InputEventMouseMotion : 
+		if is_rotating :
+			curr_moved_object.rotate(Vector3.RIGHT, event.relative.y * rotate_sensitivity)
+			curr_moved_object.rotate(Vector3.UP, event.relative.x * rotate_sensitivity)
+		else :
+			virtual_cursor_pos += event.relative
